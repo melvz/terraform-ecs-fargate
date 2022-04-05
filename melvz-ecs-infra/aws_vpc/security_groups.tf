cb75bc79b84ffe2ed29f6893300309bc3f160fc9
Melvz <raymundmelvinchua@gmail.com>
2022-04-05 10:32:01 +0800

    Update security_groups.tf

--- melvz-ecs-infra/aws_vpc/security_groups.tf
+++ melvz-ecs-infra/aws_vpc/security_groups.tf
@@ -6,24 +6,12 @@ resource "aws_security_group" "ecs_sg" {
   description = format("Security group for the %s ECS nodes", var.tag_project_name)
   vpc_id      = module.vpc.vpc_id
 
-  
-
-  // Allow Global HTTP Access into port 80
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = format("Global %s service HTTP port Ingress Access", var.tag_project_name)
-  }
-
 
   // Allow Global HTTP Access into port 8080
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
     description = format("Global %s service HTTP port Ingress Access", var.tag_project_name)
   }
     
@@ -47,10 +35,38 @@ resource "aws_security_group" "ecs_sg" {
   }
 
   tags = {
-    Name        = format("trivutmarketplace-%s-ecs-sg", var.tag_project_name)
+    Name        = format("melvin-%s-ecs-sg", var.tag_project_name)
     Project     = var.tag_project_name
     Environment = "Service"
     Workspace   = terraform.workspace
     Managed_by  = "Terraform"
+  }
+}
+
+################################################################################
+# Security Group for the LoadBalancer
+################################################################################
+resource "aws_security_group" "alb_sg" {
+  name        = format("trivutmarketplace-%s-ecs-sg", var.tag_project_name)
+  description = format("Security group for the %s ECS nodes", var.tag_project_name)
+  vpc_id      = module.vpc.vpc_id
+
+  
+
+  // Allow Global HTTP Access into port 80
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = format("Global %s service HTTP port Ingress Access", var.tag_project_name)
+  }
+
+  // Allow Global Outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
