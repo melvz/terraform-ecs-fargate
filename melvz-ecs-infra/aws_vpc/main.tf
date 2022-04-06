e27003a5d2a4ea637a3df95a4085a9cdac450ab5
Melvz <raymundmelvinchua@gmail.com>
2022-04-06 10:56:55 +0800

    Update main.tf

--- melvz-ecs-infra/aws_vpc/main.tf
+++ melvz-ecs-infra/aws_vpc/main.tf
@@ -3,7 +3,6 @@
 #-------------------------------
 provider "aws" {
   region  = var.aws_region
-  #version = "~> 2.65.0"
 }
 
 #-------------------------------
@@ -11,52 +10,11 @@ provider "aws" {
 #-------------------------------
 terraform {
   backend "s3" {
-    bucket         = "statebucket-trivut-marketplace"
-    key            = "trivut-marketplace-vpc.tfstate"
+    bucket         = "statebucket-melvz"
+    key            = "melvz-vpc.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "symbol-tf-lock"
+    dynamodb_table = "melvz-tf-lock"
   }
-  
-}
-
-
-#
-#
-#
-data "aws_caller_identity" "current" {}
-
-resource "aws_kms_key" "cmk" {
-  description = "master key"
-
-  policy = <<EOF
-{
-  "Version" : "2012-10-17",
-  "Id" : "key-default-1",
-  "Statement" : [ {
-      "Sid" : "Enable IAM User Permissions",
-      "Effect" : "Allow",
-      "Principal" : {
-        "AWS" : "arn:aws:iam::data.aws_caller_identity.current.account_id:trivut-aws-melvin"
-      },
-      "Action" : "kms:*",
-      "Resource" : "*"
-    },
-    {
-      "Effect": "Allow",
-      "Principal": { "Service": "logs.var.region.amazonaws.com" },
-      "Action": [ 
-        "kms:Encrypt*",
-        "kms:Decrypt*",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:Describe*"
-      ],
-      "Resource": "*"
-    }  
-  ]
-}
-EOF
-
   
 }
 
